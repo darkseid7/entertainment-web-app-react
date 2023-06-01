89f52229f8e814e420751409cb0b86de100d44da
Sergio andres oquendo salazar <oliveratom11@hotmail.com>
2023-05-31 23:07:32 -0500

    added search bar

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -1,32 +1,25 @@
 import { useParams, useNavigate, useSearchParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import Recommend from "../Home/components/Recommend";
+import CardList from "../../components/CardList";
 import Card from "../../components/Card";
 import useFetch from "../../hooks/useFetch";
 
 function Results() {
   const { data } = useFetch();
-  // const { title } = useParams();
-  const [params] = useSearchParams();
-  const [result, setResult] = useState([]);
+  const { title } = useParams();
 
-  useEffect(() => {
-    const filteredData = data.filter((item) => {
-      const filter = item.title.toLowerCase().includes(params.get("title"));
-      return filter;
-    });
-    setResult(filteredData);
-    console.log("se ejecuta?");
-  }, [data, params]);
-
-  console.log(result);
+  const filteredData = data.filter((item) => {
+    const filter = item.title.toLowerCase().includes(title);
+    return filter;
+  });
 
   return (
-    <Recommend>
-      {result.map((cardData, cardIndex) => (
-        <Card key={cardIndex} cardData={cardData} />
+    <CardList>
+      {filteredData.map((cardData, cardIndex) => (
+        <Card key={cardIndex} cardData={cardData} imgUrl={} />
       ))}
-    </Recommend>
+    </CardList>
   );
 }
 
